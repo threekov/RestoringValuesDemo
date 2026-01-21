a77b02e7162de835f203de1e5133bc3e2125967d
threekov <161291961+threekov@users.noreply.github.com>
2026-01-21 23:24:01 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+provider "openstack" {
+  auth_url    = var.auth_url
+  tenant_name = var.tenant_name
+  user_name   = var.user_name
+  password    = var.password
+}
+
+resource "openstack_compute_keypair_v2" "knn_key" {
+  name       = "knn-key"
+  public_key = var.public_ssh_key
+}
+
+resource "openstack_compute_instance_v2" "knn_vm" {
+  name        = "knn-vm-${var.environment}"
+  image_name  = var.image_name
+  flavor_name = var.flavor_name
+  key_pair    = openstack_compute_keypair_v2.knn_key.name
+
+  network {
+    name = var.network_name
+  }
+}
